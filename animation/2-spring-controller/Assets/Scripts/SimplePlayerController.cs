ab1fe785bc872d03ca5bf3fe77aac0778470a051
Stefan Webb <info@stefanwebb.me>
2025-06-29 12:41:58 -0700

    spring controller works!

--- animation/2-spring-controller/Assets/Scripts/SimplePlayerController.cs
+++ animation/2-spring-controller/Assets/Scripts/SimplePlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,9 +12,15 @@ namespace StefanWebb
         [SerializeField]
         private float scaleVelocity = 1f;
 
+        [SerializeField]
+        private float springHalflife = 0.1f;
+
         private Vector2 velocity = Vector2.zero;
         private Vector2 velocityTarget = Vector2.zero;
         private Vector2 acceleration = Vector2.zero;
+
+        private float deltaTimeAcc = 0f;
+        private float playerUpdatePeriod = 0.033f;
 
         void Start()
         {
@@ -22,39 +29,83 @@ namespace StefanWebb
 
         void Update()
         {
-            // Set velocity according to joystick position
-            // TODO: Compare this to adding callback
-            Vector2 inputDirection = inputManager.PlayerControls.Player.Move.ReadValue<Vector2>();
-            velocity = scaleVelocity * inputDirection;
+            deltaTimeAcc += Time.deltaTime;
 
-            // Update object position
-            transform.localPosition += Time.deltaTime * new Vector3(velocity.x, 0, velocity.y);
-
-            // TODO: Rotate object based on velocity direction
-            if (inputDirection.magnitude > 0.001)
+            if (deltaTimeAcc >= playerUpdatePeriod)
             {
-                transform.forward = new Vector3(velocity.x, 0, velocity.y).normalized;
+                /*
+                Decouple frequency of frame update and player state update.
+
+                TODO: Handle case when frame rate is too low by performing
+                multiple numerical integration steps.
+                */
+
+                // DEBUG
+                Debug.Log($"Pos: {transform.localPosition}, Vel: {velocity}, Acc: {acceleration}");
+
+                // TODO: Factor out this block into fn
+
+
+                // Set target velocity according to joystick position
+                // TODO: Compare this to adding callback
+                Vector2 inputDirection = inputManager.PlayerControls.Player.Move.ReadValue<Vector2>();
+                velocityTarget = scaleVelocity * inputDirection;
+
+                (Vector2 deltaPosition, Vector2 newVelocity, Vector2 newAcceleration) = critically_damped_spring_controller_2d(Vector2.zero, velocity, acceleration, velocityTarget, springHalflife, deltaTimeAcc);
+
+                // Update object state
+                transform.localPosition += new Vector3(deltaPosition.x, 0, deltaPosition.y);
+                velocity = newVelocity;
+                acceleration = newAcceleration;
+
+
+                deltaTimeAcc = 0;
+
+                // TODO: Rotate object based on velocity direction
+                if (velocity.magnitude > 0.001 * scaleVelocity)
+                {
+                    transform.forward = new Vector3(velocity.x, 0, velocity.y).normalized;
+                }
             }
         }
 
-        // void spring_character_update(
-        //     float& x,
-        //     float& v,
-        //     float& a,
-        //     float v_goal,
-        //     float halflife,
-        //     float dt)
-        // {
-        //     float y = halflife_to_damping(halflife) / 2.0f;
-        //     float j0 = v - v_goal;
-        //     float j1 = a + j0 * y;
-        //     float eydt = fast_negexp(y * dt);
+        float halflife_to_damping(float halflife, float eps = 1e-5f)
+        {
+            return (4.0f * 0.69314718056f) / (halflife + eps);
+        }
 
-        //     x = eydt * (((-j1) / (y * y)) + ((-j0 - j1 * dt) / y)) +
-        //         (j1 / (y * y)) + j0 / y + v_goal * dt + x;
-        //     v = eydt * (j0 + j1 * dt) + v_goal;
-        //     a = eydt * (a - j1 * y * dt);
-        // }
+        float fast_negexp(float x)
+        {
+            return 1.0f / (1.0f + x + 0.48f * x * x + 0.235f * x * x * x);
+        }
+
+        (Vector2, Vector2, Vector2) critically_damped_spring_controller_2d(
+            Vector2 position,
+            Vector2 velocity,
+            Vector2 acceleration,
+            Vector2 velocityTarget,
+            float halflife,
+            float deltaTime)
+        {
+            /*
+                Applies the force of a critically damped free spring to the displacement of the current velocity to the target velocity.
+
+                Returns the change in position, velocity, and acceleration.
+
+                TODO: Move this function into a physics script
+            */
+
+            float lambda = halflife_to_damping(halflife) / 2.0f;
+            Vector2 c1 = velocity - velocityTarget;
+            Vector2 c2 = acceleration + c1 * lambda;
+            float eydt = fast_negexp(lambda * deltaTime);
+
+            Vector2 newPosition = eydt * (((-c2) / (lambda * lambda)) + ((-c1 - c2 * deltaTime) / lambda)) +
+                (c2 / (lambda * lambda)) + c1 / lambda + velocityTarget * deltaTime + position;
+            Vector2 newVelocity = eydt * (c1 + c2 * deltaTime) + velocityTarget;
+            Vector2 newAcceleration = eydt * (acceleration - c2 * lambda * deltaTime);
+            return (newPosition, newVelocity, newAcceleration);
+        }
 
         // void FixedUpdate()
         // {
@@ -73,7 +124,11 @@ namespace StefanWebb
 
             // Handles.ArrowHandleCap(0, transform.position, transform.rotation, 0.5f, EventType.Repaint);
 
-            // Visualize velocity
+            // Visualize target velocity
+            Handles.color = Color.green;
+            Handles.DrawLine(transform.position, transform.position + new Vector3(velocityTarget.x, 0, velocityTarget.y), 2f);
+
+            // Visualize actual velocity
             Handles.color = Color.yellow;
             Handles.DrawLine(transform.position, transform.position + new Vector3(velocity.x, 0, velocity.y), 2f);
 
